89be23dd1af0c32ecc9531aa71ad34161f9c55fa
mustafa <mustafa@jotform.com>
2016-04-04 01:07:11 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as salim from 'asdjkalsd';
+import { haci } from 'tt';
+import { four as five } from 'three';
+import { six, seven } from 'eight';
+import { one, two as three } from 'five';
+import one, {two} from 'four';
+
+export class test extends Component {
+  static propTypes =                {
+    adana 
+      :    React.PropTypes.number.ashdjasd,
+    hebele: React.PropTypes.number,
+    karaka: React.PropTypes.oneOf({})
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+var rcc = function() {
+
+}
+
+rcc.propTypes = {
+  samet: React.PropTypes.number
+}
